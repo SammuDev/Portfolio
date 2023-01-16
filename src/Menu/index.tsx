d497c1210de214d775cc746976c6614c5c03a252
SammuDev <samucas878@gmail.com>
2023-01-16 15:17:32 -0300

    feat: Criando e organizando diretórios e arquivos

---
+++ src/Menu/index.tsx
@@ -0,0 +1,7 @@
+const Menu = () => {
+  return (
+    <div>Menu</div>
+  );
+};
+
+export default Menu;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import { createGlobalStyle } from 'styled-components';
+import Menu from './Menu';
+
+const GlobalStyled = createGlobalStyle`
+  * {
+    font-family: Helvetica, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu/>
+      <Routes>
+        <Route path='/' element={<App />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
