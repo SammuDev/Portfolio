4a0b9f7aa5424755d63a18298a702880a1d65003
SammuDev <samucas878@gmail.com>
2023-01-21 16:56:52 -0300

    feat: React-Icons

--- src/Home/components/Footer/Footer.tsx
+++ src/Home/components/Footer/Footer.tsx
@@ -1,15 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LinkEmail from "./styles/LinkEmail";
 import LinkWhats from "./styles/LinkWhats";
 import SectionFooter from "./styles/SectionFooter";
+
+import { FaWhatsapp } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   return (
     <SectionFooter>
       <div>
         <div>
-          ✉ <LinkEmail href='mailto:samucas878@gmail.com'>samucas878@gmail.com</LinkEmail><br/>
-          {/* <a href="https://api.whatsapp.com/send?phone=+5579988042570" target={'_blank'}>(79) 9.8804-2570</a> */}
-          <LinkWhats href='https://wa.me/5579988042570' target={'_blank'}>+55 (79) 9.8804-2570</LinkWhats>
+          <LinkEmail href='mailto:samucas878@gmail.com'><FaEnvelope/> samucas878@gmail.com</LinkEmail><br/>
+          <LinkWhats href='https://wa.me/5579988042570' target={'_blank'}><FaWhatsapp/> +55 (79) 9.8804-2570</LinkWhats>
         </div>
 
         <div></div>
