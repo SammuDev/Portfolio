5478e25595318f0b36b096a97b0b6fa014a8eb71
SammuDev <samucas878@gmail.com>
2023-01-23 17:47:46 -0300

    style: Alinhando os links socias e rotas do Menu

--- src/Menu/styles/MenuMobile.tsx
+++ src/Menu/styles/MenuMobile.tsx
@@ -6,6 +6,7 @@ interface Props {
 
 const MenuMobile = styled.div<Props>`
   display: ${props => props.on ? 'grid' : 'none'};
+  grid-template-columns: 4fr 1fr;
   align-items: center;
 
   a {
--- src/Menu/styles/RoutesMobile.tsx
+++ src/Menu/styles/RoutesMobile.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const RoutesMobile = styled.div`
-  border: 1px solid blue;
+  text-align: center;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
 `;
 
 export default RoutesMobile;
