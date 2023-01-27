d468b253856131d7c3d75a537a20a151d8472a6c
SammuDev <samucas878@gmail.com>
2023-01-27 13:23:30 -0300

    style: Animando o menu mobile

--- src/Menu/styles/MenuMobile.tsx
+++ src/Menu/styles/MenuMobile.tsx
@@ -5,14 +5,23 @@ interface Props {
 }
 
 const MenuMobile = styled.div<Props>`
+  @keyframes displayOn {
+    from {top: -100px;}
+    to {top: 0;}
+  }
+
   height: ${props => props.on ? '200px' : '0'};
   display: ${props => props.on && 'grid'};
   grid-template-columns: 1fr;
   align-items: center;
-  transition: .5s ease;
+  transition: .2s ease;
 
   div {
+    width: 25%;
+    margin: auto;
+    position: relative;
     display: ${props => !props.on && 'none'};
+    animation: ${props => props.on && 'displayOn .5s ease'};
   }
 
   @media screen and (min-width: 1020px) {
