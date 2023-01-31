5c79535ab820bf8c756f259f538fa7b555089a96
SammuDev <samucas878@gmail.com>
2023-01-31 09:42:47 -0300

    feat: Adicionando uma nova competência = MongoDB

--- src/Home/components/AboutMe/AboutMe.tsx
+++ src/Home/components/AboutMe/AboutMe.tsx
@@ -9,12 +9,13 @@ const AboutMe: React.FC = () => {
   const cards = [
     {id: 1, src:'./imgSkills/git.jpg', alt:'Logo do Git'},
     {id: 2, src:'./imgSkills/github.jpg', alt:'Logo do GitHub'},
-    {id: 3, src:'./imgSkills/react.jpg', alt:'Logo do TypeScript'},
+    {id: 3, src:'./imgSkills/react.jpg', alt:'Logo do React Js'},
     {id: 4, src:'./imgSkills/styled.jpg', alt:'Logo do Styled-Components'},
-    {id: 5, src:'./imgSkills/typescript.jpg', alt:'Logo do Next.Js'},
-    {id: 6, src:'./imgSkills/next.jpg', alt:'Logo do Node.Js'},
-    {id: 7, src:'./imgSkills/node.jpg', alt:'Logo do Express'},
-    {id: 8, src:'./imgSkills/express.jpg', alt:'Logo do React.Js'}
+    {id: 5, src:'./imgSkills/typescript.jpg', alt:'Logo do TypeScript'},
+    {id: 6, src:'./imgSkills/next.jpg', alt:'Logo do Next Js'},
+    {id: 7, src:'./imgSkills/node.jpg', alt:'Logo do Node Js'},
+    {id: 8, src:'./imgSkills/express.jpg', alt:'Logo de Express com Node'},
+    {id: 8, src:'./imgSkills/mongoDB.jpg', alt:'Logo do MongoDB'}
   ];
 
   return (
